1bc5202092aba8eb93b0e96e1e7da2fe6c6f1e60
kobakakzu0429 <kobakazu10@gmail.com>
2018-09-24 19:18:04 +0900

    add enumWeek

--- utils.js
+++ utils.js
@@ -4,3 +4,16 @@ exports.getTimeJST = () => {
   if (process.env.TZ === 'Asia/Tokyo') return date;
   return new Date(date.getTime() + 9 * 60 * 60 * 1000);
 };
+
+exports.enumWeek = str => {
+  const _enum = {
+    月: 10,
+    火: 20,
+    水: 30,
+    木: 40,
+    金: 50,
+    土: 60,
+    日: 70
+  };
+  return _enum[str];
+};
