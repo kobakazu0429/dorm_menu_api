d169c010ffcf9a53ff19413d253d77ee56f26df3
kobakakzu0429 <kobakazu10@gmail.com>
2018-09-27 21:10:31 +0900

    fix jst

--- utils.js
+++ utils.js
@@ -1,6 +1,8 @@
 exports.getTimeJST = () => {
   const moment = require('moment-timezone');
-  return moment().tz('Asia/Tokyo');
+  return moment()
+    .utc()
+    .add(9, 'h');
 };
 
 exports.enumWeek = str => {
