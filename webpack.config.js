598f3601a877fa860d8ca091ac0d278b91362901
kobakakzu0429 <kobakazu10@gmail.com>
2019-04-01 15:09:00 +0900

    create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+
+  entry: {
+    server: "./src/server.ts"
+  },
+
+  target: "node",
+
+  externals: [
+    nodeExternals()
+  ],
+
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "build")
+  },
+
+  plugins: [
+    new CleanWebpackPlugin("build")
+  ],
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts"],
+    modules: ["node_modules"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      }
+    ]
+  }
+};
