d472c9cf16311f3794850a2c620f3cd50309d359
kobakakzu0429 <kobakazu10@gmail.com>
2019-04-06 18:29:50 +0900

    create cron

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { createServer } from "http";
+import { scheduleJob } from "node-schedule";
 
 import { app } from "./app";
 import sequelize from "./sequelize";
@@ -8,7 +9,18 @@ const port = process.env.PORT || 3000;
 (async () => {
   await sequelize.sync();
 
-  createServer(app).listen(port, () =>
-    console.info(`You can see here: http://localhost:${port}`)
-  );
+  createServer(app).listen(port, () => {
+    console.info(`You can see here: http://localhost:${port}`);
+
+    scheduleJob(
+      {
+        second: 0,
+        minute: 0,
+        hour: 0
+      },
+      () => {
+        console.log("This is Cron.");
+      }
+    );
+  });
 })();
