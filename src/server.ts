214f558e48dee81402e7bc3d7e98e06b532aa964
kobakakzu0429 <kobakazu10@gmail.com>
2019-04-06 18:14:21 +0900

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import { createServer } from "http";
+
 import { app } from "./app";
 import sequelize from "./sequelize";
 
 const port = process.env.PORT || 3000;
 
 (async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.sync();
 
   createServer(app).listen(port, () =>
     console.info(`You can see here: http://localhost:${port}`)
