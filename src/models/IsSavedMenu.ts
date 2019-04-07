910ff084e40cd41c8a3b214527222bde45188a02
kobakakzu0429 <kobakazu10@gmail.com>
2019-04-07 12:00:38 +0900

    add getter of isTrue by target yera/month

--- src/models/IsSavedMenu.ts
+++ src/models/IsSavedMenu.ts
@@ -6,8 +6,33 @@ import {
   UpdatedAt
 } from "sequelize-typescript";
 
+interface IMenu {
+  id: number;
+  year: number;
+  month: number;
+  isSaved: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 @Table({ modelName: "is_saved_menu" })
 export class IsSavedMenu extends Model<IsSavedMenu> {
+  public static async isSavedTargetMonth(options: {
+    year: number;
+    month: number;
+  }) {
+    const result = (await IsSavedMenu.findOne({
+      where: {
+        year: options.year,
+        month: options.month
+      }
+    }).toJSON()) as IMenu;
+
+    if (result === null) return false;
+
+    return result.isSaved;
+  }
+
   @Column public year!: number;
   @Column public month!: number;
   @Column public isSaved!: boolean;
