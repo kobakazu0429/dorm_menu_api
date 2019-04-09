7f2f9f7bf61b8e73eda260821146cab76558d20b
kobakakzu0429 <kobakazu10@gmail.com>
2019-04-09 22:50:37 +0900

    fix dinner type

--- src/models/menu.ts
+++ src/models/menu.ts
@@ -13,9 +13,7 @@ export class Menu extends Model<Menu> {
   @Column public date!: number;
   @Column public morning!: string;
   @Column public lunch!: string;
-  @Column public dinnerA!: string;
-  @Column public dinnerB!: string;
-  @Column public dinnerAB!: string;
+  @Column public dinner!: string;
 
   @CreatedAt @Column public readonly createdAt!: Date;
   @UpdatedAt @Column public readonly updatedAt!: Date;
