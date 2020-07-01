1d3fb7a9eae7abe724437aa61c4a9bdcfea982bd
kobakakzu0429 <kobakazu10@gmail.com>
2020-07-01 10:27:00 +0900

    [test:parser] remote file

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -7,6 +7,19 @@ describe("parser", () => {
     const parsedData = await parsePDF2Json(
       path.resolve("./fixtures/201909.pdf")
     );
+
+    const expectData = await fs.readFile(
+      path.resolve("./fixtures/201909-out.json"),
+      "utf8"
+    );
+    expect(parsedData).toStrictEqual(JSON.parse(expectData));
+  });
+
+  test("remote pdf file", async () => {
+    const parsedData = await parsePDF2Json(
+      "https://raw.githubusercontent.com/kobakazu0429/dorm_menu_api/test/fixtures/201909.pdf"
+    );
+
     const expectData = await fs.readFile(
       path.resolve("./fixtures/201909-out.json"),
       "utf8"
